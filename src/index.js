b70bbaf4161e885dea17f3397bb446cc6efae6c3
DanielloXXI <cybiran_ez@mail.ru>
2023-04-13 22:51:14 +0300

    auth

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { HashRouter, BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="https://danielloxxi.github.io/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
