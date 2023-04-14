0c0e94c3a41ec9b39995fbeef8d9a969e99da10e
DanielloXXI <cybiran_ez@mail.ru>
2023-04-14 11:58:09 +0300

    auth

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { HashRouter, BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename='/https://danielloxxi.github.io/react-mesto-auth/'>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
